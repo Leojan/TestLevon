c61401a7572c3ec0faca19ec6e81829b090ea7ad
Leojan <lnikoghosyan@gmail.com>
2015-09-18 14:28:25 +0400

    Initial Commit

---
+++ LevonTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LevonTest
+//
+//  Created by Levon Nikoghosyan on 9/18/15.
+//  Copyright © 2015 Levon Nikoghosyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LevonTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LevonTest
+//
+//  Created by Levon Nikoghosyan on 9/18/15.
+//  Copyright © 2015 Levon Nikoghosyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
